6a9033bf5abafe3f330ac7e80afb4ee802873704
AlexanderPortland <alexanderportland1@gmail.com>
2025-12-04 11:57:01 -0500

    more alu ops

--- practice/dALU.v
+++ practice/dALU.v
@@ -1,24 +1,44 @@
+`define OP_OR  4'd1
+`define OP_AND 4'd2
+`define OP_XOR 4'd3
+`define OP_NOT 4'd4
+`define OP_ADD 4'd5
+`define OP_SHL 4'd6
+
+
+
 // a stab at an 8-bit ALU
 module dALU(
     input [7:0] A, B, 
-    input [1:0] op,
-    output [7:0] out, 
+    input [3:0] op,
+    output reg [7:0] out, 
     output zero
     // output negative,
-    // output overflow, 
+    // output overflow,
     // output carry // TODO: is carry not the same as overflow?
 );
     // do all the calculations
-    wire [7:0] or_res, and_res, add_res;
+    wire [7:0] or_res, and_res, not_res, xor_res;
     byte_bitwise_or or1(A, B, or_res);
     byte_bitwise_and and1(A, B, and_res);
-    byte_ripple_adder add1(A, B, carry, add_res);
+    byte_bitwise_not not1(A, not_res);
+    byte_bitwise_xor xor1(A, B, xor_res);
     
+    wire [7:0] add_res;
+    byte_ripple_adder add1(A, B, carry, add_res);
+
     // multiplex them all into the output together
-    wire [7:0] null_or, and_add;
-    byte_multiplexer bm1(8'b0, or_res, op[0], null_or);
-    byte_multiplexer bm2(and_res, add_res, op[0], and_add);
-    byte_multiplexer bm3(null_or, and_add, op[1], out);
+    always @(*) begin
+        case (op)
+            `OP_OR: out = or_res;
+            `OP_AND: out = and_res;
+            `OP_XOR: out = xor_res;
+            `OP_NOT: out = not_res;
+            `OP_ADD: out = add_res;
+            `OP_SHL: out = A << 1; // TODO: try from logic gates
+            default: $error("IE: unknown op code %d", op);
+        endcase
+    end
 
     // check if the output is zero
     wire any_bit_set;
@@ -26,19 +46,14 @@ module dALU(
     not n(zero, any_bit_set);
 endmodule
 
-// OPS:
-// 0. [RESERVED]
-// 1. or
-// 2. and
-// 3. add
-
 // TODO: ops 
-// - [ ] shift
-// - [ ] 
+// - [ ] shift (from gates)
+// - [ ] shr
+// - [ ] more tests
 
 module dALU_tb;
     reg [7:0] test_A, test_B;
-    reg [1:0] test_op;
+    reg [3:0] test_op;
     wire [7:0] test_out;
     wire test_zero;
 
@@ -46,38 +61,49 @@ module dALU_tb;
 
     task assert_byte_eq;
         input [7:0] given, expected;
+        input [8*50:1] label;
         begin
             if (given != expected) begin
-                $error("FAIL: expected byte %b, found byte %b", given, expected);
+                $error("FAIL: expected bit %b, found bit %b \t (%0s)", expected, given, label);
             end
         end
     endtask
 
     task assert_bit_eq;
         input given, expected;
+        input [8*50:1] label;
         begin
             if (given != expected) begin
-                $error("FAIL: expected bit %b, found bit %b", given, expected);
+                $error("FAIL: expected bit %b, found bit %b \t (%0s)", expected, given, label);
             end
         end
     endtask
 
     initial begin
-        assign test_op = 1; // or op code
+        assign test_op = `OP_OR; // or op code
         assign test_A = 8'b010; assign test_B = 8'b011; #10;
-        assert_byte_eq(test_out, 8'b0011); assert_bit_eq(test_zero, 0);
+        assert_byte_eq(test_out, 8'b0011, "simple or"); 
+        assert_bit_eq(test_zero, 0, "or non-zero");
 
-        assign test_op = 2; // and op code
+        assign test_op = `OP_AND; // and op code
         assign test_A = 8'b010; assign test_B = 8'b011; #10;
-        assert_byte_eq(test_out, 8'b0010); assert_bit_eq(test_zero, 0);
+        assert_byte_eq(test_out, 8'b0010, "simple and"); 
+        assert_bit_eq(test_zero, 0, "and non-zero");
 
-        assign test_op = 3; // or op code
+        assign test_op = `OP_ADD; // or op code
         assign test_A = 8'b010; assign test_B = 8'b011; #10;
-        assert_byte_eq(test_out, 8'b0101); assert_bit_eq(test_zero, 0);
+        assert_byte_eq(test_out, 8'b0101, "simple add"); 
+        assert_bit_eq(test_zero, 0, "or non-zero");
 
-        assign test_op = 2; // and op code
+        assign test_op = `OP_AND; // and op code
         assign test_A = 8'b010; assign test_B = 8'b101; #10;
-        assert_byte_eq(test_out, 8'b0000); assert_bit_eq(test_zero, 1);
+        assert_byte_eq(test_out, 8'b0000, "simple and to zero"); 
+        assert_bit_eq(test_zero, 1, "and results in zero");
+
+        assign test_op = `OP_SHL; // and op code
+        assign test_A = 8'b010; assign test_B = 8'b101; #10;
+        assert_byte_eq(test_out, 8'b0100, "simple shl"); 
+        assert_bit_eq(test_zero, 0, "shl non-zero");
 
         $display("[dALU]\t\t\t All tests passed!!");
     end
--- practice/logic.v
+++ practice/logic.v
@@ -1,4 +1,4 @@
-// selects from A or B based on X, sending that to out
+
 module byte_bitwise_and(input [7:0] A, B, output [7:0] out);
     genvar i;
     generate
@@ -26,11 +26,11 @@ module byte_bitwise_not(input [7:0] A, output [7:0] out);
     endgenerate
 endmodule
 
-module byte_bitwise_xor(input [7:0] A, output [7:0] out);
+module byte_bitwise_xor(input [7:0] A, B, output [7:0] out);
     genvar i;
     generate
         for (i = 0; i < 8; i = i + 1) begin
-            xor x(out[i], A[i]);
+            xor x(out[i], A[i], B[i]);
         end
     endgenerate
 endmodule
@@ -47,4 +47,17 @@ module byte_any_bit_set(input [7:0] A, output out);
     endgenerate
 
     assign out = temp[8];
+endmodule
+
+module byte_logical_left_shift(input [7:0] A, output [7:0] out);
+    wire zero;
+
+    assign out[0] = 0;
+
+    genvar i;
+    generate
+        for (i = 1; i < 8; i = i + 1) begin
+            assign out[i] = A[i - 1];
+        end
+    endgenerate
 endmodule
